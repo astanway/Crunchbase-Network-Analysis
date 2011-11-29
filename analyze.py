bb47341e4f3f61e9751babd200d1fc81fb4d1ed2
Abe Stanway <abestanway@gmail.com>
2011-11-29 15:05:25 -0500

    begin analyze

---
+++ analyze.py
@@ -0,0 +1,28 @@
+from networkx import *
+import matplotlib
+import matplotlib.pyplot as plt
+from operator import itemgetter
+
+f = open('investments.gml', 'r')
+gml = f.read()
+gml = gml.split('\n')[1:]
+G = parse_gml(gml, relabel=False)
+print "parsed"
+ 
+nG = nx.number_of_nodes(nx.connected_component_subgraphs(G)[0])
+nodes = nx.number_of_nodes(G)
+edges = nx.number_of_edges(G)
+print nodes
+print edges
+
+f = open('shared_founders.gml', 'r')
+gml = f.read()
+gml = gml.split('\n')[1:]
+G = parse_gml(gml, relabel=False)
+print "parsed"
+ 
+nG = nx.number_of_nodes(nx.connected_component_subgraphs(G)[0])
+nodes = nx.number_of_nodes(G)
+edges = nx.number_of_edges(G)
+print nodes
+print edges
